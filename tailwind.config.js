de666639d45ba481bbae377bf28c86e5cb76f33b
Shourav Hasan <shouravhasanshurjo8201@gmail.com>
2026-06-05 16:38:20 +0600

    Refactor motion imports to PascalCase in ProductNotFound and SignUp components to comply with ESLint rules; remove unused 'watch' mapping in SignUp component; update tailwind.config.js for improved structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
+import daisyui from "daisyui"
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  plugins: [require("daisyui")],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
   daisyui: {
     themes: ["light", "dark"],
   },
